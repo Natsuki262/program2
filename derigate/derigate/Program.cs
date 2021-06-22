ff74d17a922be35eb0910deda4cf7fdd6e5d3842
NatsukiAlen <hiro.58.fgl.2525.9552@icloud.com>
2021-06-22 18:14:39 +0900

    derigate

---
+++ derigate/derigate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace derigate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
